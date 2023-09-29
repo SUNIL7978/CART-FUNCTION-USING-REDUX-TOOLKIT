efd4a2fd6c16b622b2cc6b9c44e68046e7961de5
SUNIL7978 <Sunilprusty16@gmail.com>
2023-09-29 12:05:35 +0530

    change in app js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Modal from './components/Modal';
 
 function App() {
   const dispatch = useDispatch();
-  const { cartItems, isLoading } = useSelector((store) => store.cart);
+  const { cartItems } = useSelector((store) => store.cart);
   const { isOpen } = useSelector((store) => store.modal);
 
   useEffect(() => {
